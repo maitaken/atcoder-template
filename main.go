e07ffd5f8b925f0a3b206fc8c5b988d5f4169971
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2020-08-21 14:52:00 +0900

    Fixed : int64用の関数を削除

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 )
 
 const (
-	BUFSIZE   = 10000000
-	MOD       = 1000000007
-	INF_INT   = math.MaxInt32
-	INF_INT64 = math.MaxInt64
+	BUFSIZE = 10000000
+	MOD     = 1000000007
+	INT_INF = math.MaxInt32
 )
 
 var rdr *bufio.Reader
@@ -52,15 +51,6 @@ func readIntSlice() []int {
 	return slice
 }
 
-func readInt64Slice(size int) []int64 {
-	slice := make([]int64, size)
-	lines := strings.Split(readline(), " ")
-	for i, v := range lines {
-		slice[i] = s2i64(v)
-	}
-	return slice
-}
-
 func readint() int {
 	return s2i(readline())
 }
@@ -78,53 +68,6 @@ func readint3() (int, int, int) {
 func readint4() (int, int, int, int) {
 	lines := strings.Split(readline(), " ")
 	return s2i(lines[0]), s2i(lines[1]), s2i(lines[2]), s2i(lines[3])
-}
-
-// For int64
-func readint64() int64 {
-	return s2i64(readline())
-}
-
-func b2i64(b bool) int64 {
-	if b {
-		return 1
-	}
-	return 0
-}
-
-func abs64(v int64) int64 {
-	if v < 0 {
-		return -v
-	}
-	return v
-}
-
-func min64(values ...int64) int64 {
-	ret := int64(INF_INT64)
-	for _, v := range values {
-		if ret > v {
-			ret = v
-		}
-	}
-	return ret
-}
-
-func max64(values ...int64) int64 {
-	ret := -int64(INF_INT64)
-	for _, v := range values {
-		if ret < v {
-			ret = v
-		}
-	}
-	return ret
-}
-
-func s2i64(s string) int64 {
-	v, ok := strconv.ParseInt(s, 10, 64)
-	if ok != nil {
-		panic("Faild : " + s + " can't convert to int64")
-	}
-	return v
 }
 
 // For int
@@ -166,7 +109,7 @@ func modpow(x, y int) int {
 }
 
 func min(values ...int) int {
-	ret := INF_INT
+	ret := INT_INF
 	for _, v := range values {
 		if ret > v {
 			ret = v
@@ -176,7 +119,7 @@ func min(values ...int) int {
 }
 
 func max(values ...int) int {
-	ret := -INF_INT
+	ret := -INT_INF
 	for _, v := range values {
 		if ret < v {
 			ret = v
